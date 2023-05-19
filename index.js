95e2fe7fddb4b8430ab0116a2e1d54c928c40bce
R7558 <rupsanaworld1004@gmail.co>
2023-05-19 22:17:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9avspzh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(){
+try{
+    const appointmentOptionCollection = client.db('docPortal').collection('availableOption');
+    app.get('/availableOption', async (req, res) => {
+        const date = req.query.date;
+        const query = {};
+        const options = await appointmentOptionCollection.find(query).toArray();
+        res.send(options)
+    })
+        
+}
+finally{
+
+}
+}
+run ().catch(console.log)
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal server is running');
+})
+
+app.listen(port, () => console.log(`Doctors portal running on ${port}`))
